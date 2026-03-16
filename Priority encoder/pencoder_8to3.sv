3da372308935a6de7725c401bf505bd1d6dec3b3
Niya Maria <niyamaria23@gmail.com>
2026-03-16 17:33:11 +0530

    Clean up header comments in pencoder_8to3.sv
    Removed header comments from pencoder_8to3.sv.

--- Priority encoder/pencoder_8to3.sv
+++ Priority encoder/pencoder_8to3.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 15:50:36
-// Design Name: 
-// Module Name: pencoder_8to3
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module pencoder_8to3(y,a,v);
 input logic [7:0] y;
