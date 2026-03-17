04a24b2bb17a6989900299fe9b1f83ece3636e9e
Niya Maria <niyamaria23@gmail.com>
2026-03-17 20:26:45 +0530

    Clean up comments in decoder_3to8_tb.sv
    Removed header comments from the decoder testbench file.

--- Decoder/decoder_3to8_tb.sv
+++ Decoder/decoder_3to8_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 18:16:05
-// Design Name: 
-// Module Name: decoder_3to8_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module decoder_3to8_tb;
 parameter w = 3;
