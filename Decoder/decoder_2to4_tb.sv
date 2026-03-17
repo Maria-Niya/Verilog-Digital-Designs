323184dd9842c56ac45b831062c4e5cebe57f6db
Niya Maria <niyamaria23@gmail.com>
2026-03-17 20:26:24 +0530

    Clean up comments in decoder_2to4_tb.sv
    Removed header comments from the testbench file.

--- Decoder/decoder_2to4_tb.sv
+++ Decoder/decoder_2to4_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 18:23:16
-// Design Name: 
-// Module Name: decoder_2to4_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module decoder_2to4_tb;
 parameter w = 2;
