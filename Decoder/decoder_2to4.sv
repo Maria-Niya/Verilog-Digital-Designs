39ed599f5bf97f38cacb959453e3fee1dc49f658
Niya Maria <niyamaria23@gmail.com>
2026-03-17 20:22:22 +0530

    Clean up header comments in decoder_2to4.sv
    Removed header comments from decoder_2to4.sv

--- Decoder/decoder_2to4.sv
+++ Decoder/decoder_2to4.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 18:22:13
-// Design Name: 
-// Module Name: decoder_2to4
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module decoder_2to4#(parameter w =2) (a,y);
 input logic [w-1:0] a;
