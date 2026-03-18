d6c3bb110460cbc993ad96aa54c0ff48da86b72a
Niya Maria <niyamaria23@gmail.com>
2026-03-18 20:47:19 +0530

    Clean up comments in demux18_tb.sv
    Removed unnecessary comments and header from the testbench file.

--- Demultiplexer/demux18_tb.sv
+++ Demultiplexer/demux18_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.11.2025 14:54:06
-// Design Name: 
-// Module Name: demux18_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module demux18_tb;
 reg din;
