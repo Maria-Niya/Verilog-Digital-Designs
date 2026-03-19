80b8cdb889312dfe38bb7c59a702111c93aa4658
Niya Maria <niyamaria23@gmail.com>
2026-03-19 15:57:47 +0530

    Clean up testbench and add test cases
    Removed commented metadata and added test cases for values 5 and 9.

--- Hexadecimal 7 segment display decoder/hex_7_seg_disp_decoder_tb.sv
+++ Hexadecimal 7 segment display decoder/hex_7_seg_disp_decoder_tb.sv
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.11.2025 18:36:03
-// Design Name: 
-// Module Name: hex_7_seg_disp_decoder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module hex_7_seg_disp_decoder_tb;
 reg [3:0] a;
 wire [6:0] y;
@@ -33,5 +12,7 @@ a=4'b0000;
 #100 a=4'b0100;
 #100 a=4'b0111;
 #100 a=4'b1110;
+#100 a=4'b0101;
+#100 a=4'b1001;
 end
 endmodule
