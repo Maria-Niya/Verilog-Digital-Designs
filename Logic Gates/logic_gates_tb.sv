643c69d345bb6f7fbc1a79955233f76322ba0928
Niya Maria <niyamaria23@gmail.com>
2026-03-20 21:45:51 +0530

    Clean up comments in logic_gates_tb.sv
    Removed header comments from the testbench file.

--- Logic Gates/logic_gates_tb.sv
+++ Logic Gates/logic_gates_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.11.2025 17:04:48
-// Design Name: 
-// Module Name: logic_gates_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module logic_gates_tb;
 reg a,b;
