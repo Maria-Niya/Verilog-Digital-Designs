0d075b1a571450b738ac5e924e5cb740d307f665
Niya Maria <niyamaria23@gmail.com>
2026-03-21 19:09:27 +0530

    Clean up comments in tristatebuffer_tb.sv
    Removed unnecessary comments and header information from the testbench file.

--- Tristate Buffer/tristatebuffer_tb.sv
+++ Tristate Buffer/tristatebuffer_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 15:27:08
-// Design Name: 
-// Module Name: tristatebuffer_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tristatebuffer_tb;
 reg a,e;
