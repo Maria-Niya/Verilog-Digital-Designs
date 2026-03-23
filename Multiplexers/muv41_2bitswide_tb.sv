280beaa234af7bf063dcb39cea1e36ec1b3a993b
Niya Maria <niyamaria23@gmail.com>
2026-03-23 21:07:16 +0530

    Clean up header comments in testbench
    Removed header comments and unnecessary lines.

--- Multiplexers/muv41_2bitswide_tb.sv
+++ Multiplexers/muv41_2bitswide_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.11.2025 18:08:02
-// Design Name: 
-// Module Name: muv41_2bitswide_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module muv41_2bitswide_tb;
 reg [1:0]a,b,c,d,s;
