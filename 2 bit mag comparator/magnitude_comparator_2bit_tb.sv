b0ea2c9f890623be2dc801f0fef3663644fa6d88
Niya Maria <niyamaria23@gmail.com>
2026-03-23 21:07:54 +0530

    Clean up comments in magnitude_comparator_2bit_tb
    Removed unnecessary comments and metadata from the testbench.

--- 2 bit mag comparator/magnitude_comparator_2bit_tb.sv
+++ 2 bit mag comparator/magnitude_comparator_2bit_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.11.2025 16:25:39
-// Design Name: 
-// Module Name: magnitude_comparator_2bit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module magnitude_comparator_2bit_tb;
 reg [1:0]a,b;
