b84680b802b6346885b2f3906e4c00a034a78c1c
Niya Maria <niyamaria23@gmail.com>
2026-03-23 21:07:34 +0530

    Update mux21dataflow.sv

--- Multiplexers/mux21dataflow.sv
+++ Multiplexers/mux21dataflow.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 14:46:40
-// Design Name: 
-// Module Name: mux21dataflow
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux21dataflow(a,b,s,y);
 input logic a,b,s;
