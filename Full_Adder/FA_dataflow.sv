568651ddeb8e9e1512735e4f5fe7ae1e1c4771a0
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:31:16 +0530

    Update FA_dataflow.sv

--- Full_Adder/FA_dataflow.sv
+++ Full_Adder/FA_dataflow.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.10.2025 18:35:39
-// Design Name: 
-// Module Name: FA_dataflow
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module FA_dataflow(a,b,c,sum,carry);
