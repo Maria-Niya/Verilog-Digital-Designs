174051d6e9775ce64d5d72b174397417973bde69
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:20:36 +0530

    Clean up header comments in mux41.sv
    Removed header comments from mux41.sv file.

--- Multiplexers/mux41.sv
+++ Multiplexers/mux41.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 14:11:47
-// Design Name: 
-// Module Name: mux41
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux41(a, b, c, d, s1, s0, y); 
 input logic a, b, c, d;
