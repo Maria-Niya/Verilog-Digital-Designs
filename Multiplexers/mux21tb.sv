7e300a4ebc11e4712e7ffe2eeb10eeaa13f4f87c
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:20:09 +0530

    Clean up comments in mux21tb.sv
    Removed header comments from mux21tb.sv

--- Multiplexers/mux21tb.sv
+++ Multiplexers/mux21tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 14:47:54
-// Design Name: 
-// Module Name: mux21tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux21tb;
 reg a,b,s;
