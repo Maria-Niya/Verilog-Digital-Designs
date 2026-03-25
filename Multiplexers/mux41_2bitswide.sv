13cbcc06b3a59939a5ed3d21ffccfc0ca09f9f74
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:22:29 +0530

    Clean up comments in mux41_2bitswide.sv
    Removed header comments and unnecessary lines.

--- Multiplexers/mux41_2bitswide.sv
+++ Multiplexers/mux41_2bitswide.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.11.2025 18:06:36
-// Design Name: 
-// Module Name: mux41_2bitswide
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux41_2bitswide(a,b,c,d,s,y);
 input logic [1:0]a,b,c,d,s;
