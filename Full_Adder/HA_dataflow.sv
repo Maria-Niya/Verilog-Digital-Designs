963e5406429cbef1f93440a88d2c6c3e69c22f2d
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:29:49 +0530

    Clean up header comments in HA_dataflow.sv
    Removed header comments from HA_dataflow.sv.

--- Full_Adder/HA_dataflow.sv
+++ Full_Adder/HA_dataflow.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.11.2025 21:39:07
-// Design Name: 
-// Module Name: HA_dataflow
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module HA_dataflow(a,b,sum,carry);
