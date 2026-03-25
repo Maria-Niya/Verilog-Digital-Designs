75e23ada61db1b618d61d87da4aa161b259a0edc
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:29:34 +0530

    Update HS_tb.sv

--- Full_Adder/HS_tb.sv
+++ Full_Adder/HS_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.11.2025 18:39:06
-// Design Name: 
-// Module Name: HS_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module HS_tb;
 reg a,b;
