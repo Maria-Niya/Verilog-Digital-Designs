dece4527ad9f49cf35bdcb43ed93ade747622368
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:31:01 +0530

    Update FA_struct_withHA.sv

--- Full_Adder/FA_struct_withHA.sv
+++ Full_Adder/FA_struct_withHA.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.11.2025 21:43:45
-// Design Name: 
-// Module Name: FA_struct_withHA
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module FA_struct_withHA(a,b,c,sum,carry);
