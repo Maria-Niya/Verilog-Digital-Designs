c0cae75277aa6dea779ce78a7575a1e92f3ead4a
Niya Maria <niyamaria23@gmail.com>
2026-03-25 22:27:10 +0530

    Clean up comments in fa_41mux.sv
    Removed header comments from fa_41mux.sv.

--- Full_Adder/fa_41mux.sv
+++ Full_Adder/fa_41mux.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2025 18:50:01
-// Design Name: 
-// Module Name: fa_41mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fa_41mux(a,b,cin,s,cout);
 input logic a,b,cin;
