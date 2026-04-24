ab40737ac569f276b69c46acd09290f907c4f5b2
Niya Maria <niyamaria23@gmail.com>
2026-04-24 20:12:01 +0530

    Add files via upload

---
+++ SR latch and flip flop/sr_ff.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.11.2025 21:24:43
+// Design Name: 
+// Module Name: sr_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sr_ff(
+
+    );
+endmodule
