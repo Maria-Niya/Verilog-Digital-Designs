6fab0375ccead73e1bec5b086e8c17c38570da2f
Niya Maria <niyamaria23@gmail.com>
2026-04-24 20:11:06 +0530

    Update sr_latch.sv

--- SR latch and flip flop/sr_latch.sv
+++ SR latch and flip flop/sr_latch.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 20:26:41
-// Design Name: 
-// Module Name: sr_latch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sr_latch(input logic s,r,
                 output logic q,Q);
