93cfb32753846c90ce6e53e089641dd0203f9dce
Niya Maria <niyamaria23@gmail.com>
2026-05-05 16:29:57 +0530

    Update d_latch.sv

--- D latch and FF/d_latch.sv
+++ D latch and FF/d_latch.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 20:25:48
-// Design Name: 
-// Module Name: d_latch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module d_latch(input logic d,en,output logic q);
