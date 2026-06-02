51c8a679063a4af96dcd913f5f0583f3ed6aa890
Niya Maria <niyamaria23@gmail.com>
2026-06-02 14:54:18 +0530

    Update ff_tb.sv

--- Flip Flop/ff_tb.sv
+++ Flip Flop/ff_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 21:18:13
-// Design Name: 
-// Module Name: ff_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ff_tb;
 reg clk,d;
