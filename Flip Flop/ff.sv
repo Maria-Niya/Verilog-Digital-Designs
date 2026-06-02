757cd86625602934700b5e03cadd2fa013e1d79f
Niya Maria <niyamaria23@gmail.com>
2026-06-02 14:53:54 +0530

    Update ff.sv

--- Flip Flop/ff.sv
+++ Flip Flop/ff.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 21:29:39
-// Design Name: 
-// Module Name: ff
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ff(clk,reset,d,q);
 input logic clk,reset,d;
