64a18378492886ebd35d6e9c0abfae612f743d1d
Niya Maria <niyamaria23@gmail.com>
2026-06-03 16:12:53 +0530

    Clean up header comments in ff_with_reset.sv
    Removed header comments from ff_with_reset.sv

--- Flip Flop/ff_with_reset.sv
+++ Flip Flop/ff_with_reset.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 16:20:17
-// Design Name: 
-// Module Name: ff_with_reset
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ff_with_reset(clk,reset,d,q,Q);
 input logic clk,reset,d;
