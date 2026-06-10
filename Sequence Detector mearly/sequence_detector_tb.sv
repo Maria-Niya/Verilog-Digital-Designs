da6556477162edd36eca5ee763f2a85913406c7e
Niya Maria <niyamaria23@gmail.com>
2026-06-10 13:41:52 +0530

    Clean up comments in sequence_detector_tb.sv
    Removed header comments from sequence_detector_tb.sv

--- Sequence Detector mearly/sequence_detector_tb.sv
+++ Sequence Detector mearly/sequence_detector_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 18:07:14
-// Design Name: 
-// Module Name: sequence_detector_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sequence_detector_tb;
 reg a,reset,clk;
