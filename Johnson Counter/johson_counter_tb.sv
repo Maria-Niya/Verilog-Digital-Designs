f4bf01a8c4b03dd77e48fa7da2be0ad9d487bcbd
Niya Maria <niyamaria23@gmail.com>
2026-06-17 10:14:02 +0530

    Clean up comments in johson_counter_tb.sv
    Removed header comments from the testbench file.

--- Johnson Counter/johson_counter_tb.sv
+++ Johnson Counter/johson_counter_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 21:28:49
-// Design Name: 
-// Module Name: johson_counter_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module johson_counter_tb;
 reg clk, reset;
