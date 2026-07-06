5d73863d2c7d62d8f0c9bdca35f136c43bac95eb
Niya Maria <niyamaria23@gmail.com>
2026-07-06 16:24:40 +0530

    Add files via upload

---
+++ MOD Counter/MOD.sv
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.11.2025 22:41:51
+// Design Name: 
+// Module Name: MOD
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MOD#(parameter MAX = 5)(clk,reset,count);
+input logic clk,reset;
+output logic [3:0] count;
+logic [24:0] sclk=25'b0;
+always_ff @(posedge clk)
+sclk <= sclk + 1;
+always_ff @(posedge sclk)
+begin
+if(reset)
+count <= 4'b0000;
+else
+begin
+if (count == (MAX-1))
+count <= 4'b0000;
+else
+count = count + 1'b1;
+end
+end
+endmodule
