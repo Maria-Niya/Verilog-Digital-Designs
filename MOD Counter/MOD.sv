d8c771716f6d3a2b5642c9a6d60d949495c45167
Niya Maria <niyamaria23@gmail.com>
2026-07-07 16:53:13 +0530

    Update MOD.sv

--- MOD Counter/MOD.sv
+++ MOD Counter/MOD.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2025 22:41:51
-// Design Name: 
-// Module Name: MOD
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module MOD#(parameter MAX = 5)(clk,reset,count);
 input logic clk,reset;
